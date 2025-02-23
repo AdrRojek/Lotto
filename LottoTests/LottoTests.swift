60d30403bbc296167a818f2f49ccd9805d358179
Adrian Rojek <72630248+AdrRojek@users.noreply.github.com>
2025-02-23 21:01:28 +0100

    Initial Commit

---
+++ LottoTests/LottoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LottoTests.swift
+//  LottoTests
+//
+//  Created by adrian on 23/02/2025.
+//
+
+import Testing
+@testable import Lotto
+
+struct LottoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
